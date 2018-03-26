00373c0fe9f02ec0b6b2c22e9c3f2465f0c0f9f5
Muqsith Irfan <mui@one.com>
2018-03-26 17:44:03 +0400

    Made documentation bit better

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # performance-wrapper
 
+
 ##### A wrapper to obtain performance(only time) for methods (functions within object)
 
 
-Inject start-time and end-time calls your methods dynamically without modifying your own code. This library works for the methods that take only one callback or return promises or none. Check unit test for more details.
 
-`
+Inject start-time and end-time calls for your methods dynamically without modifying your own code. This library works for the methods that take only one callback or return promises or none. Check unit test for more details.
 
+
+##### Example
+
+
+code
+
+
+```javascript
 
     const applyPerformanceWrapper = require('performance-wrapper');
 
@@ -14,11 +22,60 @@ Inject start-time and end-time calls your methods dynamically without modifying 
         console.log(functionName, util.inspect(args), startTime, endTime);
     }
 
-    const myObj = applyPerformanceWrapper(new MyObject(1,2,3), 'myObj', logger);
+    class MyObject {
+        constructor() {
+            // dummy conctructor
+        }
+
+        callbackSum(a,b, cb) {
+            cb(a+b);
+        }
+
+        promisedSum(a,b) {
+            return (
+                new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve(a+b);
+                    }, 100);
+                })
+            );
+        }
+
+        async asyncSum(a,b) {
+            return await this.promisedSum(a,b);
+        }
+
+        sum(a,b) {
+            return a+b;
+        }
+    }
+
+    const myObj = applyPerformanceWrapper(new MyObject(), 'myObj', logger);
+    console.log('normal: ', myObj.sum(3, 5));
+    myObj.callbackSum(3, 5, (result) => {
+        console.log('callback: ', result);
+    });
+
+    myObj.promisedSum(3, 5)
+    .then((result) => {
+        console.log('promise: ', result);
+    });
+
+    myObj.asyncSum(3, 5)
+    .then((result) => {
+        console.log('async: ', result);
+    });
+```
 
 
-
-`
+This library exports a default function called applyPerformanceWrapper, this function receives three paramaters
+- object: the methods of this obect are intercepted for performance logging
+- name: name for the object, this name appended to the returned function name in logging
+- logger: this is a function that receives the performance details of the function under observation
+    -- name : name of the function under observation, eg: myObj.sum
+    -- arguments: arguments to the function under observation, eg: a,b
+    -- start time: start time of the function under observation
+    -- end time: end time of the function under observation
 
 
 The passed callback function (logger in the above case) is called asynchronously, without affecting the stack of the function under observation.
--- test/unit-test.js
+++ test/unit-test.js
@@ -1,77 +1,51 @@
-const crypto = require('crypto'),
-    util = require('util'),
-    applyPerformanceWrapper = require('../index')
-    ;
-class MoreDummy {
-
-    constructor(a, b, c) {
-        console.log(a, b, c);
-    }
-
-    op0(str) {
-        const hash = crypto.createHmac('sha256', 'secret')
-                   .update(str)
-                   .digest('hex');
-        return hash;
-    }
-
-    op1(options, cb) {
-        let result = ['a', 'b', 'c', 'd', 'e'];
-        setTimeout(() => {
-            cb(result);
-        }, 200);
-    }
-
-    op2(opt1, opt2, cb) {
-        let result = ['1', '2', '3', '4', '5', '6'];
-        setTimeout(() => {
-            cb(result);
-        }, 100);
-    }
-
-    op3(opt1) {
-        return (
-            new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (((Math.random() * 100) | 0) % 2) {
-                        resolve('heheheh ...');
-                    } else {
-                        reject(new Error('booo booo'));
-                    }
-                }, 1000);
-            })
-        )
-    }
-}
-
-class Dummy extends MoreDummy {
-    constructor(a, b, c) {
-        super(a, b, c)
-    }
-}
+const util = require('util'),
+    applyPerformanceWrapper = require('../index');
 
 function logger({functionName, args, startTime, endTime}) {
     console.log(functionName, util.inspect(args), startTime, endTime);
 }
 
-describe('test applyPerformanceWrapper', function() {
-    it('#applyPerformanceWrapper', function(done) {
-        // creation of obj
-    const obj = applyPerformanceWrapper(new Dummy(1,2,3), 'obj', logger);
+class MyObject {
+    constructor() {
+        // dummy conctructor
+    }
 
-    // users of obj
-    console.log(obj.op0('muqsith'));
-    obj.op1('select * from persons', console.log);
-    obj.op2(null, null, console.log);
-    obj.op3('promise')
-    .then((res) => {
-        console.log(res);
-        done();
-    })
-    .catch((err) => {
-        console.log(err);
-        done();
-    });
+    callbackSum(a,b, cb) {
+        cb(a+b);
+    }
 
-    })
-})
+    promisedSum(a,b) {
+        return (
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(a+b);
+                }, 100);
+            })
+        );
+    }
+
+    async asyncSum(a,b) {
+        return await this.promisedSum(a,b);
+    }
+
+    sum(a,b) {
+        return a+b;
+    }
+}
+
+const myObj = applyPerformanceWrapper(new MyObject(), 'myObj', logger);
+
+console.log('normal: ', myObj.sum(3, 5));
+myObj.callbackSum(3, 5, (result) => {
+    console.log('callback: ', result);
+});
+
+myObj.promisedSum(3, 5)
+.then((result) => {
+    console.log('promise: ', result);
+});
+
+myObj.asyncSum(3, 5)
+.then((result) => {
+    console.log('async: ', result);
+});
